226de60db455fd8c6b988e244f86983a14aacea2
Mihkel Haava <mhaava@users.noreply.github.com>
2017-11-30 09:41:19 +0200

    test

--- index.php
+++ index.php
@@ -10,18 +10,9 @@
 	</head>
 	<body>
 		<header>
-<<<<<<< HEAD
-			<div class="header-left"></div>
-			<div class="header-right"></div>
-=======
-<<<<<<< HEAD
-			<div class="header-left"></div>
-			<div class="header-right"></div>
-=======
 			<div class="header-left">left</div>
 			<div class="header-right">right</div>
->>>>>>> 5a6c4b73c603cd703f77390048b77ff676a5340b
->>>>>>> c4f1b99f0c1c610f5e2f10f97a9e212b562fdbea
+
 		</header>
 
 	</body>
