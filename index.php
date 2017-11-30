7808a15b165c33dfb8caf451ee7a50468f32eef4
Mihkel Haava <mhaava@users.noreply.github.com>
2017-11-30 09:02:01 +0200

    2

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" ></meta>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+
+
+	</body>
+</html>
