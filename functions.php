99c82dbc6aafd2017525ef8bc48b47285d607243
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-11-30 10:47:09 +0200

    functions muudetud

--- functions.php
+++ functions.php
@@ -1,3 +1,26 @@
 <?php
-
+	//lisada kaustad
+	require("../../config.php");
+	$database = "if17_mihkel_2";
+	$photo_dir = "uploads/";
+	$thumb_dir = "thumbnails/";
+	
+	//muuta hiljem muutujad vastavaks
+	function displayImages(){
+		$html = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT filename, thumbnail FROM pildid");
+		$stmt->bind_result($filename, $thumbnail);
+		$stmt->execute();
+		//kõik pisipildid
+		while ($stmt->fetch()){
+			$html .= '<img src="thubnails/'. $thumbnail .'">'
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		echo $html;
+	}
+	
+	
 ?>
