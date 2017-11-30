180b14dc676e8b0d3c5b016bbc1030421ed2cdeb
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-11-30 11:30:19 +0200

    lisatud

--- fullimage.php
+++ fullimage.php
@@ -1,5 +1,26 @@
 <?php
-$idea = ($_GET["id"]);
-echo $idea;
-echo "test";
+$id = ($_GET["id"]);
+
+
+	require("../../config.php");
+	$database = "if17_mihkel_2";
+	$photo_dir = "uploads/";
+	$thumb_dir = "thumbnails/";
+	
+	//muuta hiljem muutujad vastavaks
+		$html = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT filename FROM pildid WHERE id=?");
+		$stmt->bind_param("i", $id);
+		$stmt->bind_result($filename);
+		$stmt->execute();
+		//kõik pisipildid
+		while ($stmt->fetch()){
+			$html .= '<img src="uploads/'. $filename .'">';
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		echo $html;
+	
 ?>
