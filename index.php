302e1d79ff9e3a6b6f53c22d60af2a9243df28e0
Marii Helena Keerig <mariihelena@users.noreply.github.com>
2017-12-07 09:58:50 +0200

    täiendatud

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require ("functions.php");
 	</head>
 	<body>
 		<header>
-			<div class="header-left">mingi logo siia nt</div>
+			<div class="header-left"><img src="graphics/pealehelogo.png" alt="Logo" height="42" ></div>
 			<div class="header-right"><a href="upload.php">Photo upload!</a></div>
 		</header>
 		<br>
